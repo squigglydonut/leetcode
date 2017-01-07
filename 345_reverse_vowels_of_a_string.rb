16724e2a8a3ab56fc4b2816408bac0a08244f3ed
Zach <zacharymichaelmoore@gmail.com>
2017-01-07 13:24:25 -0500

    leetcode 345

---
+++ 345_reverse_vowels_of_a_string.rb
@@ -0,0 +1,24 @@
+def reverse_vowel(string)
+  vowels = %w(a e i o u)
+  # consonants = %w(b c d f g h j k l m n p q r s t v w x y z)
+  only_vowels_array_reversed = []
+  string.each_char {|l| only_vowels_array_reversed << l if vowels.include?(l)}
+  only_vowels_array_reversed = only_vowels_array_reversed.reverse
+
+  idx = 0
+  idx1 = 0
+
+  while idx < string.length
+    if vowels.include?(string[idx])
+      string[idx] = only_vowels_array_reversed[idx1]
+      idx += 1
+      idx1 += 1
+    else
+      idx += 1
+    end
+  end
+
+    string
+end
+
+p reverse_vowel("leetcode")
